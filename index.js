564b05032567cf967313b236476d3d12d3447782
Prashant Kumar Prajapati <kprashant694@gmail.com>
2017-03-22 22:44:47 +0530

    Server Created

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var express = require('express');
-var exphbs = require('express-handlebars');
-var bodyParser = require('body-parser');
+//var exphbs = require('express-handlebars');
+//var bodyParser = require('body-parser');
+var path = require('path');
+
+var index = require('./routes/index');
 
 var app = express();
 
-app.set('views',path.join(__dirname,'views'));
-app.engine('handlebars',exphbs({defaultLayout:'layout'}));
-app.set('view engine','handlebars');
+//app.set('views',path.join(__dirname,'views'));
+//app.engine('handlebars',exphbs({defaultLayout:'layout'}));
+//app.set('view engine','handlebars');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
 
-//app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/',index);
 
 app.set('port',4000);
 
