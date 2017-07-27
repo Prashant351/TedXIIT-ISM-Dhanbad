b5a5c71616e244cdfcfb221f8f07989fa5d582f5
Rahul-Sridhar <rahul.sridhar97@yahoo.com>
2017-07-27 23:22:16 +0530

    Blog data scraped

--- routes/blog.js
+++ routes/blog.js
@@ -13,27 +13,33 @@ app.get('/', function(req, res) {
       console.log("Getting data");
       var $ = cheerio.load(html);
       var img_attr, title, timestamp, content;
-      var json = {
+      var jsn = {
         img_attr: "",
         title: "",
         timestamp: "",
         content: ""
       };
-      $('#post-182').filter(function() {
-        console.log('avsdz');
-        var data = $(this);
-        img_attr = data.children().eq(0).children().children().attr('src');
-        title = data.children().eq(1).children().eq(0).children().eq(0).children('a').text();
-        timestamp = data.children().eq(1).children().eq(0).children().eq(1).children().eq(0).children().children('time').eq(0).text();
-        content = data.children().eq(1).children().eq(1).children('p').text();
-        json.img_attr = img_attr;
-        json.title = title;
-        json.timestamp = timestamp;
-        json.content = content;
+      var json_array = [];
+      $('article').each(function() {
+        $(this).filter(function() {
+          console.log('avsdz');
+          var data = $(this);
+          img_attr = data.children().eq(0).children().children().attr('src');
+          title = data.children().eq(1).children().eq(0).children().eq(0).children('a').text();
+          timestamp = data.children().eq(1).children().eq(0).children().eq(1).children().eq(0).children().children('time').eq(0).text();
+          content = data.children().eq(1).children().eq(1).children('p').text();
+          jsn = {
+            img_attr: img_attr,
+            title: title,
+            timestamp: timestamp,
+            content: content
+          };
+          json_array.push(jsn);
+        });
       });
     }
     console.log("dasoj");
-    fs.writeFile('data.json', JSON.stringify(json), function(err) {
+    fs.writeFile('data.json', JSON.stringify(json_array), function(err) {
       console.log("Data Updated");
     });
   });
